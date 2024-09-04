318b8ea041b09aa81e60abdde96be2e05c27b0d3
Cickruss <icaro.o.macedo@gmail.com>
2024-09-04 20:17:12 -0300

    feat: add validator request

--- src/curumim-api/Adapters/Inbound/Http/Models/Transaction/Lend/Validators/ValidatorLend.cs
+++ src/curumim-api/Adapters/Inbound/Http/Models/Transaction/Lend/Validators/ValidatorLend.cs
@@ -4,5 +4,9 @@ namespace curumim_api.Adapters.Inbound.Http.Models.Transaction.Lend.Validators;
 
 public class ValidatorLend: FlatValidator<RequestLend>
 {
-    
+    public ValidatorLend()
+    {
+        ErrorIf(m => m.bookId is null, m => "Não pode ser nulo ou vazio", m => m.bookId);
+        ErrorIf(m => m.userId is null, m => "Não pode ser nulo ou vazio", m => m.userId);
+    }
 }
